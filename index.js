581b930eb4d953d5ce293cf6c313ea744fb123dc
Mathieu Blanchet <mathieu.blanchet@biol.lu.se>
2023-11-23 16:33:49 +0100

    fetch api data

--- index.js
+++ index.js
@@ -2,12 +2,69 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// for API call
+const https = require('https');
+
+var listSites = [
+  {
+    "Kartblad":  "02C7H",
+    "Namn":  "ÖRTOFTA",
+    "RT90n": "",
+    "RT90o": "",
+    "WGS84_lat": "55.78919",
+    "WGS84_lon": "13.21456",
+    "Status":  "JA",
+  },
+  {
+    "Kartblad":  "02D2C",
+    "Namn":  "BLENTARP",
+    "RT90n": "",
+    "RT90o": "",
+    "WGS84_lat": "55.57256",
+    "WGS84_lon": "13.6253",
+    "Status":  "NO",
+  },
+  {
+    "Kartblad":  "02D7C",
+    "Namn":  "SEBBARP",
+    "RT90n": "",
+    "RT90o": "",
+    "WGS84_lat": "55.79694",
+    "WGS84_lon": "13.61277",
+    "Status":  "NO"
+  }
+];
+
+
+https.get('https://canmove-app.ekol.lu.se/sft/booking2.php', (resp) => {
+
+  let data = '';
+
+  // A chunk of data has been received.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    //console.log(data);
+    listSites=data;
+  });
+  
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
+
+
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   //res.send('Testing page for new fageltaxering webpage with booking map.')
 
-  res.render('pages/index', {});
+  res.render('pages/index', {
+    listSites: listSites
+  });
 
 
 })
@@ -16,5 +73,7 @@ app.listen(port, () => {
   console.log(`App http://localhost:${port}`);
 
   app.use(express.static(__dirname + '/public'));
+
+
 
 })
--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,38 @@
 
+
 $(document).ready(function () {
 
   // set the map
-  var map = L.map('map').setView([62.47204526039855, 16.149376718556645], 4);
+  var map = L.map('map').setView([62.47204526039855, 16.149376718556645], 5);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
+    
+      minZoom: 5,
+      maxZoom: 7,
       attribution: '© OpenStreetMap'
   }).addTo(map);
 
+
+  var bookedIcon = L.icon({
+      iconUrl: 'img/marker-green.png',
+      iconSize:     [8, 8], // size of the icon
+  });
+
+  var notbookedIcon = L.icon({
+      iconUrl: 'img/marker-white.png',
+      iconSize:     [8, 8], // size of the icon
+  });
+
+  var listSites=JSON.parse(decodeURIComponent($("#listSitesHidden").html()));
+
+  listSites.forEach((site) => {
+    if (site.Status== 'JA') {
+      L.marker([site.WGS84_lat, site.WGS84_lon], {icon: bookedIcon}).addTo(map).bindPopup("02C7H");
+    }
+    else {
+      L.marker([site.WGS84_lat, site.WGS84_lon], {icon: notbookedIcon}).addTo(map).bindPopup("02C7H");
+    }
+  })
 });
 
 
